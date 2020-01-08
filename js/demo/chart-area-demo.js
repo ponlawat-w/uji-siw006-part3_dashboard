3d4ec1d935e08b557ef55847ff357d7e3fed0fa5
ponlawat-w <ponlawat_w@cmu.ac.th>
2020-01-08 14:00:15 +0100

    Class Session 25630108

--- js/demo/chart-area-demo.js
+++ js/demo/chart-area-demo.js
@@ -2,53 +2,93 @@
 Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
 Chart.defaults.global.defaultFontColor = '#292b2c';
 
-// Area Chart Example
-var ctx = document.getElementById("myAreaChart");
-var myLineChart = new Chart(ctx, {
-  type: 'line',
-  data: {
-    labels: ["Mar 1", "Mar 2", "Mar 3", "Mar 4", "Mar 5", "Mar 6", "Mar 7", "Mar 8", "Mar 9", "Mar 10", "Mar 11", "Mar 12", "Mar 13"],
-    datasets: [{
-      label: "Sessions",
+$(document).ready(() => {
+  var ctx = document.getElementById("myAreaChart");
+  var myLineChart;
+
+  const createDataset = (stringDataOrArray, label, color) => {
+    const data = typeof stringDataOrArray === 'string' ?
+      stringDataOrArray.split(';').slice(2).map(x => parseInt(x)) : stringDataOrArray;
+    return {
+      label: label,
       lineTension: 0.3,
-      backgroundColor: "rgba(2,117,216,0.2)",
-      borderColor: "rgba(2,117,216,1)",
+      backgroundColor: color,
+      borderColor: color,
       pointRadius: 5,
-      pointBackgroundColor: "rgba(2,117,216,1)",
-      pointBorderColor: "rgba(255,255,255,0.8)",
+      pointBackgroundColor: color,
+      pointBorderColor: color,
       pointHoverRadius: 5,
-      pointHoverBackgroundColor: "rgba(2,117,216,1)",
+      pointHoverBackgroundColor: color,
       pointHitRadius: 50,
       pointBorderWidth: 2,
-      data: [10000, 30162, 26263, 18394, 18287, 28682, 31274, 33259, 25849, 24159, 32651, 31984, 38451],
-    }],
-  },
-  options: {
-    scales: {
-      xAxes: [{
-        time: {
-          unit: 'date'
+      data: data,
+      fill: false
+    };
+  };
+
+  const generateCharts = datasets => {
+    const max = Math.max(...datasets.map(d => d.data).flat(), 15);
+
+    myLineChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: [
+          '0-15',
+          '15-30',
+          '30-45',
+          '45-60',
+          '60-75',
+          '75-90+'
+        ],
+        datasets: datasets,
+      },
+      options: {
+        scales: {
+          xAxes: [{
+            time: {
+              unit: 'date'
+            },
+            gridLines: {
+              display: false
+            },
+            ticks: {
+              maxTicksLimit: 7
+            }
+          }],
+          yAxes: [{
+            ticks: {
+              min: 0,
+              max: max,
+              maxTicksLimit: 5
+            },
+            gridLines: {
+              color: "rgba(0, 0, 0, .125)",
+            }
+          }],
         },
-        gridLines: {
-          display: false
-        },
-        ticks: {
-          maxTicksLimit: 7
+        legend: {
+          display: true
         }
-      }],
-      yAxes: [{
-        ticks: {
-          min: 0,
-          max: 40000,
-          maxTicksLimit: 5
-        },
-        gridLines: {
-          color: "rgba(0, 0, 0, .125)",
-        }
-      }],
-    },
-    legend: {
-      display: false
+      }
+    });
+  };
+
+  $.ajax({
+    url: 'http://soccerdatastats.com/services/api.php/records/PARTIDOS?filter=ID,eq,8537974',
+    dataType: 'json',
+    success: json => {
+      const datasets = [
+        createDataset(json.records[0].goles_home_total_MARCADOS, 'Home', 'rgb(54, 162, 235)'),
+        createDataset(json.records[0].goles_away_total_MARCADOS, 'Away', 'rgb(255, 99, 132)')
+      ];
+
+      const totalData = [];
+      for (let i = 0; i < datasets[0].data.length; i++) {
+        totalData.push(datasets[0].data[i] + datasets[1].data[i]);
+      }
+      datasets.push(createDataset(totalData, 'Total', 'rgb(153, 102, 255)'));
+
+      generateCharts(datasets);
     }
-  }
+  });
 });
--- js/demo/datatables-demo.js
+++ js/demo/datatables-demo.js
@@ -1,4 +1,44 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
-  $('#dataTable').DataTable();
+  const table = $('#dataTable').DataTable({
+    columns: [
+      {data: 'HOME'},
+      {data: 'AWAY'},
+      {data: 'G0_15'},
+      {data: 'G15_30'},
+      {data: 'G30_45'},
+      {data: 'G45_60'},
+      {data: 'G60_75'},
+      {data: 'G75_90'}
+    ]
+  });
+
+  const getGoalsData = string => string.split(';').slice(2).map(x => parseInt(x));
+
+  const gColumns = [
+    'G0_15',
+    'G15_30',
+    'G30_45',
+    'G45_60',
+    'G60_75',
+    'G75_90'
+  ];
+
+  $.ajax({
+    url: 'http://soccerdatastats.com/services/api.php/records/PARTIDOS?filter=FECHA,ge,20200108',
+    dataType: 'json',
+    success: json => {
+      const records = [];
+      for (let i = 0; i < json.records.length; i++) {
+        const record = json.records[i];
+        const home = getGoalsData(json.records[i].goles_home_total_MARCADOS);
+        const away = getGoalsData(json.records[i].goles_away_total_MARCADOS);
+        for (let j = 0; j < gColumns.length; j++) {
+          record[gColumns[j]] = home[j] + away[j];
+        }
+        records.push(record);
+      }
+      table.rows.add(records).draw();
+    }
+  });
 });
